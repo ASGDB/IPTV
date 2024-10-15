52e15e875d6346b4588fcb7a3b3c4c08b16348c4
ASGDB <165355662+ASGDB@users.noreply.github.com>
2024-10-15 19:11:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ source_urls = [
     "https://raw.githubusercontent.com/Moexin/IPTV/Files/IPTV.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
+    "https://live.zbds.top/tv/iptv4.txt",
+    "https://live.zbds.top/tv/iptv6.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
     "https://live.zhoujie218.top/tv/iptv6.txt",
