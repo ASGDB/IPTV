f1d7099763c37da0eae31c142efb878d3075834f
ASGDB <165355662+ASGDB@users.noreply.github.com>
2024-11-10 13:59:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ source_urls = [
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
     "https://live.zhoujie218.top/tv/iptv6.txt",
+    "https://4gtv.mytvsuper.xyz/myiptv.m3u",
     "https://tv.youdu.fan:666/live/",
     "http://ww.weidonglong.com/dsj.txt",
     "http://xhztv.top/zbc.txt",
